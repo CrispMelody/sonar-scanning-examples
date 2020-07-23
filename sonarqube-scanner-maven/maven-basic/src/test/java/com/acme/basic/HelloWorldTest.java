3cf39344ad315f9dfe5a273e8588c7ac189905f3
风辰 <lfcrispp@gmail.com>
2020-07-23 17:01:26 +0800

    test

--- sonarqube-scanner-maven/maven-basic/src/test/java/com/acme/basic/HelloWorldTest.java
+++ sonarqube-scanner-maven/maven-basic/src/test/java/com/acme/basic/HelloWorldTest.java
@@ -8,5 +8,13 @@ public class HelloWorldTest {
   public void sayHello() {
     new HelloWorld().sayHello();
   }
+  @Test
+  public void notCovered() {
+    new HelloWorld().notCovered();
+  }
+  @Test
+  public void notCovered1() {
+    new HelloWorld().notCovered1();
+  }
 
 }
